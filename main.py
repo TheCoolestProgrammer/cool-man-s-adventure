04a16577f7717b4e3b53e07e16f088fb6e071ff9
mr_goldenman <vasya.chaukin.97@mail.ru>
2021-12-27 19:47:50 +0500

    some fixes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-import copy
+
 
 class Background():
     def load_background(self,level):
@@ -14,8 +14,10 @@ class Person():
     def load_person(self,person):
         if person ==0:
             self.person = pygame.image.load("data/person.png")
+            self.person = pygame.Surface.convert_alpha(self.person)
             self.person_look_left = pygame.transform.flip(self.person,True,False)
             self.weapon = pygame.image.load("data/sword.png")
+            self.weapon = pygame.Surface.convert_alpha(self.weapon)
             self.weapon_look_left = pygame.transform.flip(self.weapon,True,False)
             self.look_right = True
             self.walk_right_animation =[]
@@ -27,8 +29,10 @@ class Person():
             self.animation_active = False
             for i in range(1,6):
                 a = pygame.image.load(f"data/person_walk_anim{i}.png")
+                a = pygame.Surface.convert_alpha(a)
                 self.walk_right_animation.append(a)
                 a = pygame.transform.flip(a,True,False)
+                a = pygame.Surface.convert_alpha(a)
                 self.walk_left_animation.append(a)
             self.tics = fps // len(self.walk_right_animation)//2
             self.counter_tics = 0
@@ -39,13 +43,17 @@ class Person():
             self.normal_weapon_punch_left_animation = []
             for i in range(1,10):
                 a = pygame.image.load(f"data/person_sword_punch{i}.png")
+                a = pygame.Surface.convert_alpha(a)
                 self.person_normal_weapon_punch_right_animation.append(a)
                 a = pygame.transform.flip(a, True, False)
+                a = pygame.Surface.convert_alpha(a)
                 self.person_normal_weapon_punch_left_animation.append(a)
             for i in range(1,10):
                 a = pygame.image.load(f"data/sword_punch{i}.png")
+                a = pygame.Surface.convert_alpha(a)
                 self.normal_weapon_punch_right_animation.append(a)
                 a = pygame.transform.flip(a, True, False)
+                a = pygame.Surface.convert_alpha(a)
                 self.normal_weapon_punch_left_animation.append(a)
 
 
@@ -53,7 +61,13 @@ class Person():
         self.position_y = screen_height-self.person.get_height()-10
         self.speed = background.speed
 
-
+    def ticks_plus(self):
+        if self.counter_tics >= self.tics:
+            self.counter_tics = 0
+            self.now_frame += 1
+            if not self.look_right:
+                pass
+        self.counter_tics += 1
     def bliting(self):
         if  not self.animation_active:
             if  self.look_right:
@@ -65,47 +79,30 @@ class Person():
         else:
             if self.walk:
                 if self.look_right:
-                    if self.counter_tics >=self.tics:
-                        self.counter_tics =0
-                        self.now_frame += 1
-                        if not self.look_right:
-                            pass
-                    self.counter_tics+=1
+                    self.ticks_plus()
                     screen.blit(self.walk_right_animation[self.now_frame%5], (self.position_x, self.position_y))
                     screen.blit(self.weapon, (self.position_x, self.position_y))
                 else:
-                    if self.counter_tics >=self.tics:
-                        self.counter_tics =0
-                        self.now_frame += 1
-                        if not self.look_right:
-                            pass
-                    self.counter_tics+=1
+                    self.ticks_plus()
+
                     screen.blit(self.walk_left_animation[self.now_frame%5], (self.position_x, self.position_y))
                     screen.blit(self.weapon_look_left, (self.position_x-(self.weapon.get_width()-self.person.get_width()), self.position_y))
 
 
             elif self.normal_weapon_punch:
                 if self.counter_tics==0:
-                    self.tics=fps//len(self.normal_weapon_punch_left_animation)
+                    # self.tics=fps//len(self.normal_weapon_punch_left_animation)
                     self.position_y -= self.normal_weapon_punch_left_animation[0].get_height() - self.person.get_height()
                     if not self.look_right:
                         self.position_x -= self.normal_weapon_punch_left_animation[0].get_width() - self.person.get_width()
                 if self.look_right:
-                    if self.counter_tics >= self.tics:
-                        self.counter_tics = 0
-                        self.now_frame += 1
-                        if not self.look_right:
-                            pass
-                    self.counter_tics += 1
+                    self.ticks_plus()
+
                     screen.blit(self.person_normal_weapon_punch_right_animation[self.now_frame % len(self.person_normal_weapon_punch_right_animation)], (self.position_x, self.position_y))
                     screen.blit(self.normal_weapon_punch_right_animation[self.now_frame%len(self.normal_weapon_punch_right_animation)], (self.position_x, self.position_y))
                 else:
-                    if self.counter_tics >= self.tics:
-                        self.counter_tics = 0
-                        self.now_frame += 1
-                        if not self.look_right:
-                            pass
-                    self.counter_tics += 1
+                    self.ticks_plus()
+
                     screen.blit(self.person_normal_weapon_punch_left_animation[self.now_frame % len(self.person_normal_weapon_punch_left_animation)], (self.position_x, self.position_y))
                     screen.blit(self.normal_weapon_punch_left_animation[self.now_frame%len(self.normal_weapon_punch_left_animation)],(self.position_x,self.position_y))
                 if self.now_frame==len(self.normal_weapon_punch_left_animation):
@@ -114,13 +111,9 @@ class Person():
                     self.counter_tics=0
                     self.animation_active=False
                     self.position_y += self.normal_weapon_punch_left_animation[0].get_height() - self.person.get_height()
+                    # self.tics = fps//len(self.walk_right_animation)
                     if not self.look_right:
                         self.position_x += self.normal_weapon_punch_left_animation[0].get_width() - self.person.get_width()
-
-
-# class Border(pygame.sprite.Sprite):
-#     def __init__(self,x1,y1,x2,y2):
-#         super().__init__(self)
 
 
 running = True
@@ -130,9 +123,6 @@ screen_width, screen_height = 1280, 720
 screen = pygame.display.set_mode((screen_width, screen_height))
 fps = 60
 clock = pygame.time.Clock()
-# font = pygame.font.SysFont("Times New Roman", 40)
-# menu_button_width = 500
-# menu_button_height = 100
 
 background = Background()
 background.load_background(0)
@@ -170,10 +160,9 @@ while running:
     elif keys[pygame.K_RIGHT]:
         background.position_x -= background.speed
 
-
-    screen.fill((0, 0, 0))
+    #screen.fill((0, 0, 0))
     background.bliting()
     person.bliting()
     pygame.display.update((0,0,screen_width,screen_height))
     clock.tick(fps)
-#просто для проверки работоспособности
+    pygame.display.set_caption(str(clock.get_fps()))
